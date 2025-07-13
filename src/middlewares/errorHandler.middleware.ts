9ec00c6320c5f3d2db8ad13dfeeec3572c0203b3
James Marques <jamzmarks@gmail.com>
2025-07-13 19:08:45 -0300

    feat: errorHandler middleware created

--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -4,7 +4,7 @@ export function errorHandler(
   err: any,
   req: Request,
   res: Response,
-  next: NextFunction
+  _: NextFunction
 ) {
   res.status(err.status || 500).json({
     error: err.message || "Internal Server Error",
