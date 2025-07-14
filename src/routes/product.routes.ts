66772b13d085ee3cd7baaba7318f061025b6af74
James Marques <jamzmarks@gmail.com>
2025-07-14 19:48:14 -0300

    feat: productRoutes created

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -6,13 +6,21 @@ const router = Router();
 const productController = new ProductController();
 
 router.get("/products", productController.getProducts);
+
+//Filter Route
+router.get("/products/filters", productController.getFilters);
+
+//Count Product with filter route
+router.get("/products/count", productController.getProductsCount);
+
+//Products CRUD routes
 router.get("/products/:id", productController.getProductById);
 router.post("/products", productController.createProduct);
 router.patch("/products/:id", productController.updateProduct);
 router.delete("/products/:id", productController.softDelete);
 
+//Soft delete routes
 router.get("/products/deleted", productController.getProductById);
 router.get("/products/deleted/:id", productController.getProductById);
-
 
 export default router;
